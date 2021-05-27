183013fc324ab67ad4d0660786ad1e2af62f159a
everglow <78077569+okchanho@users.noreply.github.com>
2021-05-27 12:12:40 +0900

    노드
    노드

---
+++ Node.js/DAY1/ch2.js
@@ -0,0 +1,8 @@
+scope 
+const global; //상수
+
+var global; //전역변수 var
+
+if(true){
+    let global; //지역변수 let 스코프 괄호 안에서만 적용 
+}
--- Node.js/myvar.js
+++ Node.js/myvar.js
@@ -1,3 +1,16 @@
+'use  strict'
+
+const obj = {
+    title : 'node.js 올인원 패키지'
+}
+
+function isEmptyObj() {    
+    if (obj.title) {
+        return true
+    }else{
+        return false
+    }
+}
 
 
-module.exports.a = "hello a"
+const isEmptyObj =() => {obj.title ? true : false} //삼항연산자
